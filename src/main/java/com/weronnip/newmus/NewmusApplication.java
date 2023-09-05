75369f8ac57d3c719ba9c329760965fb21ec45a5
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-09-05 19:46:22 +0300

    music player

--- src/main/java/com/weronnip/newmus/NewmusApplication.java
+++ src/main/java/com/weronnip/newmus/NewmusApplication.java
@@ -1,11 +1,10 @@
 package com.weronnip.newmus;
-	
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewmusApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewmusApplication.class, args);
