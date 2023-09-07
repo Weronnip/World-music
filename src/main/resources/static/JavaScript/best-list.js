d2784da3cb23930652e9b4a67086b55b14087c2c
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-09-07 18:08:56 +0300

    add button in sidebar, etid element in css

--- src/main/resources/static/JavaScript/best-list.js
+++ src/main/resources/static/JavaScript/best-list.js
@@ -10,11 +10,41 @@ let track_index = 0;
 
 const music_list = [
     {
-        name : 'Antarctica  -',
-        artist : '$uicideboy$',
+        name : 'Antarctica ',
+        artist : '- $uicideboy$',
         music : 'music/$uicideboy$ - Antarctica.mp3'
-    }
-]
+    },
+    {
+        name : 'Kill Yourself (Part III) ',
+        artist : '- $uicideboy$',
+        music : 'music/$uicideboy$ - Kill Yourself (Part III).mp3'
+    },
+    {
+        name : ' Mentalite(slowed+rewerb) ',
+        artist : '- Baby Gang',
+        music : 'music/Baby Gang - Mentalite(slowed+rewerb).mp3'
+    },
+    {
+        name : 'ZEMLYA ',
+        artist : '- EDWXRDX',
+        music : 'music/EDWXRDX - ZEMLYA.mp3'
+    },
+    {
+        name : 'Own Paradise (Slowed) ',
+        artist : '- LXAES',
+        music : 'music/LXAES - Own Paradise (Slowed).mp3'
+    },
+    {
+        name : 'Ooes - Ночь (Minus) ',
+        artist : '- LXAES',
+        music : 'music/LXAES Kkknellerstation - Ooes -  Ночь (Minus).mp3'
+    },
+    {
+        name : 'Нашла другого ',
+        artist : '- Юрий Шатунов',
+        music : 'music/Юрий Шатунов- Нашла другого.mp3'
+    },
+];
 
 loadTrack(track_index);
 
