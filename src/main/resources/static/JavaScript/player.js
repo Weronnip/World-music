b4fefb6ae2f33ed566849be868c0b9e6812acb4a
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-09-07 12:43:01 +0300

    edit element in player.js

--- src/main/resources/static/JavaScript/player.js
+++ src/main/resources/static/JavaScript/player.js
@@ -1,7 +1,5 @@
 import('../music')
 
-let now_playing = document.querySelector('.now-playing');
-let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
 let track_artist = document.querySelector('.track-artist');
 
@@ -13,8 +11,7 @@ let seek_slider = document.querySelector('.seek_slider');
 let volume_slider = document.querySelector('.volume_slider');
 let curr_time = document.querySelector('.current-time');
 let total_duration = document.querySelector('.total-duration');
-let wave = document.getElementById('wave');
-let randomIcon = document.querySelector('.fa-random');
+let randomIcon = document.querySelector('.bx-shuffle');
 let curr_track = document.createElement('audio');
 
 let track_index = 0;
@@ -25,12 +22,12 @@ let updateTimer;
 const music_list = [
     {
         name : 'Antarctica  -',
-        artist : '  $uicideboy$',
+        artist : '$uicideboy$',
         music : 'music/$uicideboy$ - Antarctica.mp3'
     },
     {
         name : 'Kill Yourself (Part III)  -',
-        artist : '  $uicideboy$',
+        artist : '$uicideboy$',
         music : 'music/$uicideboy$ - Kill Yourself (Part III).mp3'
     },
     
