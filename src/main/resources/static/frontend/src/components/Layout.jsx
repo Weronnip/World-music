d670808dc291603efb0025b63461e51ecae53ada
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-09-17 19:22:06 +0300

    changes button profile

--- src/main/resources/static/frontend/src/components/Layout.jsx
+++ src/main/resources/static/frontend/src/components/Layout.jsx
@@ -6,15 +6,15 @@ function Layout() {
         <>
             <div className="sidebar">
                 <div className="wrapper">
-                <div className="name-site">World Music</div>
+                    <div className="name-site">World Music</div>
                     <ul className="nav-list">
                         <li><a href="#" className='mini-bth'>Top charts</a></li>
                         <li><a href="#" className='mini-bth'>Library</a></li>
                         <li><a href="#" className='mini-bth'>Playlists</a></li>
+                        <div className="form">
+                            <button className='button'>Profile</button>
+                        </div>
                     </ul>
-                    <div className="form">
-                        <button className='button'>Profile</button>
-                    </div>
                 </div>
             </div>
 
