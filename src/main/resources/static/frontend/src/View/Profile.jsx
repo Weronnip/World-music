c49f9ee2ec25cdbb755146b6e182e873b107b483
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-09-21 12:28:22 +0300

    add block playlist in profile

--- src/main/resources/static/frontend/src/View/Profile.jsx
+++ src/main/resources/static/frontend/src/View/Profile.jsx
@@ -11,11 +11,13 @@ function Profile() {
                     <img src="https://sun7-19.userapi.com/impg/yiswPQIDa3ulfs5BzOKEZiOT-8by6cbdF_Jy6w/X-Gz_h3Dh4U.jpg?size=504x505&quality=95&sign=a6814ef9157484d36908adb0907140e3&type=album" 
                     alt="user_avatar" className='photo-user'/>
                     <div className="name-user">Afton Weron</div>
-                    <div className="block-now">he's listening now: To Rider</div>
-                </div>
-                <div className="library">
-
-                </div>         
+                    <div className="block-now">Now playing: To Rider</div>
+                    <div className="library-content">
+                        <div className="playlist-content">
+                            Name playlist
+                        </div>
+                    </div> 
+                </div>        
             </div>
         </div>
     );
