e4e3cc09a1885f25c89116d25b1e3903bf961af8
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-09-21 04:55:11 +0300

    add pop-up, add header on home page

--- src/main/resources/static/frontend/src/View/Home.jsx
+++ src/main/resources/static/frontend/src/View/Home.jsx
@@ -1,16 +1,37 @@
 import './css/home.css'
+import { IconName, BiSolidPlaylist } from "react-icons/bi/index";
+
 
 function Home() {
     return(
-
         <div className="section">
+            <div className="header">
+                <ul className="nav-list">
+                    <li><a href="#openModal" className='openModal' title='playlist'><BiSolidPlaylist /> </a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </div>
             <div className="wrappers">
                 <div className="playlist">
-                    <div className="name-block">One playlist</div>
-                    <div className="track-list">
-                        <div className="information-track">
-                            <div className="name-track">Name Track</div>
-                            <div className="artist-track">Track Atrist</div>
+                    <div className="modal" id='openModal'>
+                        <div className="modal_dialog">
+                            <div className="modal_content">
+                                <div className="model_header">
+                                    <div className="name-block">One playlist 
+                                    <a href="#close" title='Close' className='close'>x</a>
+                                    </div>
+                                </div>
+                                <div className="modal_body">
+                                    <div className="track-list">
+                                        <div className="information-track">
+                                            <div className="name-track">Name Track</div>
+                                            <div className="artist-track">Track Atrist</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
