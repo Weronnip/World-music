1c3e7757f61a0d379e272aeadcb1df80531f1ae9
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-09-22 10:47:26 +0300

    fixbug scroll block library

--- src/main/resources/static/frontend/src/View/Profile.jsx
+++ src/main/resources/static/frontend/src/View/Profile.jsx
@@ -5,6 +5,7 @@ import './css/profile.css';
 
 function Profile() {
     let username = "Your name"
+    let playnow = "Now playing: To Rider";
     return(
         <div className="section">
             <div className="profile-content">
@@ -12,7 +13,7 @@ function Profile() {
                     <img src="https://sun7-19.userapi.com/impg/yiswPQIDa3ulfs5BzOKEZiOT-8by6cbdF_Jy6w/X-Gz_h3Dh4U.jpg?size=504x505&quality=95&sign=a6814ef9157484d36908adb0907140e3&type=album" 
                     alt="user_avatar" className='photo-user'/>
                     <div className="name-user">{username}</div>
-                    <div className="block-now">Now playing: To Rider</div>
+                    <div className="block-now">{playnow}</div>
                     <div className="library-content">
                         <div className="playlist-content">
                             Name playlist
--- src/main/resources/static/frontend/src/components/Layout.jsx
+++ src/main/resources/static/frontend/src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import { AiOutlineSetting } from 'react-icons/ai/index';
 import { BiLike, BiHomeAlt2, BiLibrary, BiSolidPlaylist, BiMusic } from 'react-icons/bi/index'
 import './Layout.css'
 
@@ -13,6 +14,7 @@ function Layout() {
                         <li><a href="/charts" className='mini-bth'><BiLike className='icon' />Top charts</a></li>
                         <li><a href="/library" className='mini-bth'><BiLibrary className='icon'/>Library</a></li>
                         <li><a href="/playlist" className='mini-bth'><BiSolidPlaylist className='icon'/>Playlists</a></li>
+                        <li><a href="/setting" className='mini-bth'><AiOutlineSetting className='icon'/>Setting</a></li>
                         <div className="form">
                             <form action="/profile">
                                 <button className='button'>Profile</button>
